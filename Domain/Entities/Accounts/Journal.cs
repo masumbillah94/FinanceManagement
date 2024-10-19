4873e9ad25e329d4ce525e95f9fffe1a1fc7eae2
Masum Billah <masumbillahbar@gmail.com>
2024-10-19 09:04:41 +0600

    model added

---
+++ Domain/Entities/Accounts/Journal.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Journal
+    {
+        public int Id { get; set; }
+        public int CoaId { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public decimal Amount { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public virtual ChartOfAccount Account { get; set; }
+    }
+}
